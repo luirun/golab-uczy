28c697edca032e42593e0f55e457f674b75a548f
Jan Matuszewski <luirun@gazeta.pl>
2016-03-30 18:26:51 +0200

    Dodano kontakt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 LearnRails::Application.routes.draw do
+resources :contacts, only: [:new, :create]
 root to: 'visitors#new'
 end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
