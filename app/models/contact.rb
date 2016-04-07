4271c3fa8a2c9717a7d01c03771aa44a784d3489
Jan Matuszewski <luirun@gazeta.pl>
2016-04-07 17:55:22 +0200

    Baza MySql i dzialajacy kontakt

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,15 @@
 class Contact < ActiveRecord::Base
-	has_no_table
-	
-	attr_accessor :name, :string
-	attr_accessor :email, :string
-	attr_accessor :content, :string
-	
 	validates_presence_of :name
 	validates_presence_of :email
 	validates_presence_of :content
-	validates_format_of :email,
-		:with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+	validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 	validates_length_of :content, :maximum => 500
-	
+		def update_database
+
+		table_name = "contacts"
+		@name = self.name
+		@email = self.email
+		@content = self.content
+		save
+	end
 end
